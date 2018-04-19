b0eaa2443506c4bd4c0698cf76b40adde655b671
realeve@qq.com <realeve@qq.com>
2018-04-19 23:37:42 +0800

    增加星空

--- src/components/Galaxy.vue
+++ src/components/Galaxy.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="galaxy">
+    <!-- <v-star/> -->
     <div class="page-bg"></div>
     <div class="bg-dot"></div>
   </div>
@@ -7,4 +8,15 @@
 <style lang="less">
 @import "../assets/css/galaxy.less";
 </style>
+
+// <script>
+// import VStar from "../components/Star";
+
+// export default {
+//   components: {
+//     VStar
+//   }
+// };
+//
+</script>
 
---
+++ src/components/Star.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="galaxy-star" :id="startId"></div>
+</template>
+
+<style lang="less">
+.galaxy-star {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  width: 100%;
+  height: 100vh;
+}
+</style>
+<script>
+import Particles from "particles.js";
+import param from "./particlesSetting";
+export default {
+  computed: {
+    startId() {
+      return (
+        "star_" +
+        Math.random()
+          .toString(36)
+          .substring(2)
+      );
+    }
+  },
+  mounted() {
+    particlesJS(this.startId, param);
+  }
+};
+</script>
